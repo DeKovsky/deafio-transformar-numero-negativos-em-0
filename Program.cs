de0a5be6d350f382970dd78e0d027384128ba428
DeKovsky <andrewkovsky.dev@outlook.com>
2021-12-05 10:59:49 -0300

    adicionados todas os diretórios ao commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace desafio_0_para_vetores_negativos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] posicoes = { -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 };
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (posicoes[i] < 0)
+                {
+                    posicoes[i] = 0;
+                }
+            }
+
+            foreach (int p in posicoes)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,6 @@
+#Curso Técnico de Informática para Internet
+
+##Desafio: Atribuir 0 para os valores negativos de uma Array.
+
+O desafio consisti em criar uma array com números inteiros positivos e negativos e atribuir 0 para aqueles que são negativos.
+
